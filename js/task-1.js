7edf54c5a8161aa8374a55fed914ab3e91b89fde
Natalia256 <70845659+Natalia256@users.noreply.github.com>
2021-01-20 17:58:29 +0200

    Delete task-1.js

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-import users from "./users.js";
-
-// Задание 1. Получить массив имен всех пользователей (поле name).
-
-const getUserNames = users.flatMap (item => item.name);
-
-console.log(getUserNames);
-// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
