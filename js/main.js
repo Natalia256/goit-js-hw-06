fd3e40bbeb55f473d7d7e24dcca23e330a727c66
Natalia256 <70845659+Natalia256@users.noreply.github.com>
2021-01-22 13:54:30 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ console.log(getInactiveUsers(users)); // [объект Moore Hensley, объек
 // Задаие 5. Получить пользоваля (не массив) по email (поле email, он уникальный).
 
 const getUserWithEmail = (users, email) =>
-  users.filter((x) => x.email === email);
+  users.find((x) => x.email === email);
 
 console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
 console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
